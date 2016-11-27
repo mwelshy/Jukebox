7a0197cae4e09f0ef710f648125902159046494c
Michael Welsh <brohemian1989@gmail.com>
2016-11-27 16:12:04 -0500

    changed to song 1 and song 2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+
+
+
 function Jukebox() {
+
 	this.play = function() {
 		document.getElementById("music").play();
 	}
@@ -15,13 +19,16 @@ function Jukebox() {
 		document.getElementById("music").volume-=0.1;
 	}
 
-	this.nextSong = function () {
+	this.songOne = function () {
 		document.getElementById("music").src="assets/final_countdown.mp3";
 	}
-	this.previousSong = function() {
+	this.songTwo = function() {
 		document.getElementById("music").src="assets/h3h3song.mp3";
+
 	}
 }
+
+
 
 
 
@@ -38,8 +45,8 @@ document.getElementsByTagName("h1")[0].addEventListener("click", Jukebox.play);
 document.getElementsByTagName("h2")[0].addEventListener("click", Jukebox.pause);
 document.getElementsByTagName("h3")[0].addEventListener("click", Jukebox.volumeUp);
 document.getElementsByTagName("h3")[1].addEventListener("click", Jukebox.volumeDown);
-document.getElementsByTagName('h4')[0].addEventListener("click", Jukebox.nextSong);
-document.getElementsByTagName('h4')[1].addEventListener("click", Jukebox.previousSong);
+document.getElementsByTagName('h4')[0].addEventListener("click", Jukebox.songOne);
+document.getElementsByTagName('h4')[1].addEventListener("click", Jukebox.songTwo);
 
 
 
