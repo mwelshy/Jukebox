71b565cbd8e41d2f07f88c6f7833043880213695
Michael Welsh <brohemian1989@gmail.com>
2016-11-27 15:48:53 -0500

    added second song. Allowed user to choose each song they want played

--- script.js
+++ script.js
@@ -14,7 +14,21 @@ function Jukebox() {
 	this.volumeDown = function() {
 		document.getElementById("music").volume-=0.1;
 	}
+
+	this.nextSong = function () {
+		document.getElementById("music").src="assets/final_countdown.mp3";
+	}
+	this.previousSong = function() {
+		document.getElementById("music").src="assets/h3h3song.mp3";
+	}
 }
+
+
+
+
+// function nextSong() {
+// 	document.getElementById("music").src="assets/final_countdown.mp3";
+// }
 
 
 
@@ -24,6 +38,8 @@ document.getElementsByTagName("h1")[0].addEventListener("click", Jukebox.play);
 document.getElementsByTagName("h2")[0].addEventListener("click", Jukebox.pause);
 document.getElementsByTagName("h3")[0].addEventListener("click", Jukebox.volumeUp);
 document.getElementsByTagName("h3")[1].addEventListener("click", Jukebox.volumeDown);
+document.getElementsByTagName('h4')[0].addEventListener("click", Jukebox.nextSong);
+document.getElementsByTagName('h4')[1].addEventListener("click", Jukebox.previousSong);
 
 
 
