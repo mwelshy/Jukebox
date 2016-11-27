7db4d2470cdd5f814ed5886be0395ca8e7310716
Michael Welsh <brohemian1989@gmail.com>
2016-11-27 16:37:57 -0500

    added song array. Used array to play songs

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+/* Display at least one song on the page when the page loads
+Give the user the ability to play that song, without using the "built-in" play button. This could be through a different button, through clicking or mousing over an image on the page, or any other device of your choosing.
+Give the user the ability to stop that song without using the "built-in" stop button. Once again, this could be through a different button, through clicking or mousing over an image on the page, or any other device of your choosing.
+Give the user the ability to load at least one different song into the Jukebox besides the one that is loaded when the page initially renders
+The whole Jukebox should be backed by an object called Jukebox with methods to play, stop, and load songs. */
 
+mySongs = ["assets/final_countdown.mp3", "assets/h3h3song.mp3"]
 
 
 function Jukebox() {
@@ -20,10 +26,10 @@ function Jukebox() {
 	}
 
 	this.songOne = function () {
-		document.getElementById("music").src="assets/final_countdown.mp3";
+		document.getElementById("music").src= mySongs[1];
 	}
 	this.songTwo = function() {
-		document.getElementById("music").src="assets/h3h3song.mp3";
+		document.getElementById("music").src= mySongs[0];
 
 	}
 }
